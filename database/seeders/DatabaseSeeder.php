16cb49d13f4da4854ffd8cc58c1e6a9bc844a959
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-08-31 08:47:34 +0600

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'User',
+            'email' => 'user@dev.com',
+            'password' => Hash::make('user@dev.com'),
+            'email_verified_at' => now(),
         ]);
+        User::factory(50)->create();
     }
 }
